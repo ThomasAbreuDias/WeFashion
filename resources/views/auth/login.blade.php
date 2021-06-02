5eafa002d04f50ce2c6062b48113c537ee4eb3c0
Thomas Abreu Dias <abreudiasthomas@gmail.com>
2021-06-02 22:14:50 +0200

    Login tamplate rework

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('content')
     <div class="row">
@@ -8,38 +8,46 @@
             <form role="form" method="POST" action="{{ url('/login') }}">
                 {{ csrf_field() }}
 
-                <div class="input-field col s12 {{ $errors->has('email') ? ' has-error' : '' }}">
-                  <input id="email" type="email" class="validate" name="email" value="{{ old('email') }}" required>
-                  <label for="email">E-mail Address</label>
-                    @if ($errors->has('email'))
-                        <div class="col s12">
+                <div class="row {{ $errors->has('email') ? ' has-error' : '' }}">
+                    <fieldset class="input-field col s12">
+                        <input id="email" type="email" class="validate" name="email" value="{{ old('email') }}" required>
+                        <label for="email">E-mail Address</legend>
+                        @if ($errors->has('email'))
+                            <div class="col s12">
+                                <span class="red-text">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            </div>
+                        @endif
+                    </fieldset>
+                </div>
+
+                <div class="row {{ $errors->has('password') ? ' has-error' : '' }}" required>
+                    <fieldset class="input-field col s12">
+                        <input type="password" name="password" class="validate" min="8" id="password">
+                        <label for="password">Password</label>
+                        @if ($errors->has('password'))
                             <span class="red-text">
-                                <strong>{{ $errors->first('email') }}</strong>
+                                <strong>{{ $errors->first('password') }}</strong>
                             </span>
-                        </div>
-                    @endif
+                        @endif
+                    </fieldset>
                 </div>
-
-                <div class="input-field col s12 {{ $errors->has('password') ? ' has-error' : '' }}" required>
-                    <input type="password" name="password" class="validate" min="8" id="password">
-                    <label for="password">Password</label>
-                    @if ($errors->has('password'))
-                        <span class="red-text">
-                            <strong>{{ $errors->first('password') }}</strong>
-                        </span>
-                    @endif
+                <div class="row">
+                    <fieldset class="col s12 input-field">
+                        <label>
+                            <input type="checkbox" id="remember" name="remember" class="filled-in" />
+                            <span for="remember" class="control-label">Remember Me</span>
+                        </label>
+                    </fieldset>
                 </div>
-
-                <p class="col s12">
-                  <input type="checkbox" id="remember" name="remember" />
-                  <label for="remember">Remember Me</label>
-                </p>
-
-                <div class="input-field col s12">
-                    <button type="submit" class="btn waves-effect waves-light">Login</button>
-                    <p>
-                        <a class="" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
-                    </p>
+                <div class="row">
+                    <fieldset class="input-field col s12">
+                        <button type="submit" class="btn waves-effect waves-light green darken-1">Login</button>
+                        <p>
+                            <a class="" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
+                        </p>
+                    </fieldset>
                 </div>
             </form>
         </div>
